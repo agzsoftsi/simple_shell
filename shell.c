a7b77a365aceaedd691bb4a2e8bf80edc36ef5f4
agzsoftsi <agzsoftsi@gmail.com>
2019-11-18 15:47:29 +0000

    add commit to shell.c

--- shell.c
+++ shell.c
@@ -1,20 +1,10 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-extern FILE *stderr;
-extern FILE *stdin;
-extern FILE *stdout;
-#define UNUSED(argc)(void)(argc)
-
-void intoHsh(void);
-char **ParseCommand(char *command, char *separator);
-void _prompt(void);
-void _exec(char **param);
-void _free(char **param);
-
+/**
+ * main - shell init program
+ * @argc: Number of Arguments
+ * @argv: Array of arguments
+ * Return: Return 0
+ * Authors - Carlos Garcia - Ivan Dario Lasso - Cohort 10 - Cali
+ **/
 int main(int argc, char *argv[])
 {
 
