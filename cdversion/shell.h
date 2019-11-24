4ecb965db905f88844074ef6fa68d991b9ab8850
ilasso <1204@holbertonschool.com>
2019-11-24 02:31:38 +0000

    shell.h betty

--- cdversion/shell.h
+++ cdversion/shell.h
@@ -16,6 +16,8 @@
 #include <errno.h>
 #include <sys/stat.h>
 #define UNUSED(argc)(void)(argc)
+#define CHANGE_DIR 1
+#define EXIT_SHELL 2
 void intoHsh(char **env);
 char **ParseCommand(char *command, char *separator);
 void _prompt(void);
@@ -23,7 +25,7 @@ void _exec(char **param, char **env);
 void _free(char **param);
 void _printenv(char **env);
 int Builtin(char *command, char **env);
-char *_GetEnv(char *var,char **env);
+char *_GetEnv(char *var, char **env);
 void _printenviron(void);
 void _path(char **param, char **env);
 #endif
