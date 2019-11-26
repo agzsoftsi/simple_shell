2d4c3e4e33c6a3c522b8147acc56ef3bebc48e19
ilasso <1204@holbertonschool.com>
2019-11-26 14:01:30 +0000

    executes

--- errors.c
+++ errors.c
@@ -4,14 +4,15 @@
  *@program : exe shell
  *@param: Parsed command
  *@message: Desc err to print
+ *@Qexe: Quantity Executions
  * Authors - Carlos Garcia - Ivan Dario Lasso - Cohort 10 - Cali
  **/
-void errors(char *program, char **param, char *message)
+void errors(char *program, char *param, char *message, int Qexe)
 {
 
 	char buf[256];
 
-	sprintf(buf, "%s: %d: %s: %s\n", program, 1, param[0], message);
+	sprintf(buf, "%s: %d: %s: %s\n", program, Qexe, param, message);
 	write(STDERR_FILENO, &buf, strlen(buf));
 
 }
