128d20f986648a80e5c8a8b7baa870e57d557492
ilasso <1204@holbertonschool.com>
2019-11-27 12:55:21 +0000

    drop sprintf

--- errors.c
+++ errors.c
@@ -10,9 +10,10 @@
 void errors(char *program, char *param, char *message, int Qexe)
 {
 
-	char buf[256];
-
-	sprintf(buf, "%s: %d: %s: %s\n", program, Qexe, param, message);
-	write(STDERR_FILENO, &buf, _strlen(buf));
+	UNUSED(program);
+	UNUSED(param);
+	UNUSED(message);
+	UNUSED(Qexe);
+	perror("Error");
 
 }
