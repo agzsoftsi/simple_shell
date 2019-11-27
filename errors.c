6bf3506892ced3f1fb17b1c24dbd9dc6c97b5a8e
agzsoftsi <agzsoftsi@gmail.com>
2019-11-27 11:19:31 +0000

    update _strlen

--- errors.c
+++ errors.c
@@ -13,6 +13,6 @@ void errors(char *program, char *param, char *message, int Qexe)
 	char buf[256];
 
 	sprintf(buf, "%s: %d: %s: %s\n", program, Qexe, param, message);
-	write(STDERR_FILENO, &buf, strlen(buf));
+	write(STDERR_FILENO, &buf, _strlen(buf));
 
 }
